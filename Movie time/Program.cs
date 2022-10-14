418797c7818e812ede73b9174963ed0de8eb7779
dj2548 <113076714+dj2548@users.noreply.github.com>
2022-10-14 11:02:28 -0400

    Update Program.cs

--- Movie time/Program.cs
+++ Movie time/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, Welcome to BlockBusters would you like to ");
         }
     }
 }
